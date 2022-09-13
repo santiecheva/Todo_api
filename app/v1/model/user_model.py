d97632caaec88f2aad654cbdb3502be0376f3c73
Santiago Echeverri <santiecheva@gmail.com>
2022-09-13 07:05:03 -0500

    creacion de modelos

---
+++ app/v1/model/user_model.py
@@ -0,0 +1,12 @@
+from enum import unique
+import peewee
+
+from app.v1.utils.db import db 
+
+class User(peewee.Model):
+    email = peewee.CharField(unique=True, index=True)
+    username = peewee.CharField(unique=True, index=True)
+    password = peewee.CharField()
+    
+    class Meta:
+        database = db 
